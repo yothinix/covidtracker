340a8c2fb93d32924f839ed5c1195aa520336008
Yothin Muangsommuk <m@yothinix.com>
2020-04-20 22:34:40 +0700

    :wrench: Add MEDIA_ROOT and MEDIA_URL settings :rocket:

--- covidtracker/covidtracker/settings.py
+++ covidtracker/covidtracker/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
 INSTALLED_APPS += [
     'patients',
     'check',
+    'products',
 ]
 
 MIDDLEWARE = [
@@ -67,6 +68,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                # 'django.template.context_processors.media',
             ],
         },
     },
@@ -126,3 +128,6 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
 ]
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
